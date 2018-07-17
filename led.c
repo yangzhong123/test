b6397cd2b8ecd1be5d094fb735aa62586ab0d06e
YZ <YZ>
2018-07-17 20:00:44 +0800

    four led.c

--- led.c
+++ led.c
@@ -4,12 +4,16 @@ void Leds_Contrl(void)
 {
   UINT led=1;
   UINT i;
+  UINT j;
   for(i=0;i<100;i++)
   {
-	led=led*2;
-	if(led==0x00)
+	for(j=0;j<100;j++)
 	{
+	  led=led*2;
+	  if(led==0x00)
+	  {
 		led=1;
+	  }
 	}
   }
 }
